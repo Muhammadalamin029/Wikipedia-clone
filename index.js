4d87ca7ee30b5b7fced296b4bd09c7ad148c193b
Muhammadalamin029 <139736168+Muhammadalamin029@users.noreply.github.com>
2024-07-31 17:50:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=500&srsearch=`
+const searchQuery = document.querySelector("#search-info");
+
+
+async function getSearch() {
+    const response = await fetch(url + searchQuery.value)
+    const data = await response.json()
+
+
+    showSearchResult(data.query.search)
+}
+
+function showSearchResult(data) {
+    const searchResult = document.querySelector(".result-info")
+    
+    searchResult.innerHTML = ""
+    
+    data.forEach(item => {
+        const resultInfo = document.createElement("div");
+        
+        resultInfo.innerHTML = ""
+        resultInfo.className = "search-result";
+        resultInfo.innerHTML = `
+        
+        <h2>${item.title}</h2>
+        <p>${item.snippet}....</p>
+        <a href='https://en.wikipedia.org/?curid=${item.pageid}' target="blank">read more</a>
+
+        `
+
+        searchResult.appendChild(resultInfo)
+    });
+}
+
